f1612a9560fd9ccd0277f5a6eb848525c9174d3b
BungeonMaster <47398539+BungeonMaster@users.noreply.github.com>
2019-08-07 15:25:46 -0700

    Finished all of level 5 module 1

--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,55 @@
 package _02_Advanced_Robot_Race;
 
-public class AdvancedRobotRace {
-	// Re-do the robot race recipe from level 3 module 0. 
-	// This time, use threads to make all of the robots go at the same time.
-}
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class AdvancedRobotRace extends Thread {
+	
+	static final int NUM_ROBOTS = 5;
+	
+	static boolean robotReachedTop = false;
+	
+	Robot robot;
+	int position;
+	
+	
+	AdvancedRobotRace(int position) {
+		
+		this.position = position;
+		robot = new Robot();
+		robot.setY(700);
+		robot.setX(100 + position * 100);
+		
+	}
+	
+		public static void main(String[] args) {
+			
+			ArrayList<AdvancedRobotRace> race = new ArrayList<AdvancedRobotRace>();
+
+			//3. use a for loop to initialize the robots.
+			for (int i = 0; i < NUM_ROBOTS; i++) {
+				race.add(new AdvancedRobotRace(i));
+			}
+			
+			for(int i = 0;i < race.size();i++) {
+				race.get(i).start();
+			}
+		}
+		
+		public void run() {
+			while(!robotReachedTop) {
+				robot.move(new Random().nextInt(50));
+				
+				if(robot.getY() <= 0) {
+					if(!robotReachedTop) {
+						robotReachedTop = true;	
+						System.out.println("Robot " + position + " won!");
+						robot.sparkle();
+					}
+					break;
+				}
+			}
+		}
+	}
